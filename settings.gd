6534a0751f736d5d0251078f9f52819fec0f123e
despi4 <johapo62@gmail.com>
2024-10-10 21:11:17 +0500

    САММАРИ
    дуумаю это все
    ВРОДЕ ВСЕ ЧТО ОКОНЧИВАЕТСЯ НА GD ЭТО КОД , TSCN ЭТО ТИП СЦЕНА
    ДУМАЮ TSCN вы можете открыть у себя в годот

---
+++ settings.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+func _on_check_button_toggled(button_pressed):
+	if button_pressed == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
